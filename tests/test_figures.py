d2a376f9013fb9f9ae9f3f464c5182e07bfb34ac
nhcheng <57288817+nhcheng@users.noreply.github.com>
2022-11-28 16:52:53 -0500

    debug pytest

--- tests/test_figures.py
+++ tests/test_figures.py
@@ -1,6 +1,11 @@
 import os
+import sys
+
 
 def test_all():
     """Test all scripts in ./scripts/figures"""
+
+    os.system('Xvfb :1 -screen 0 1600x1200x16  &')
+    os.environ['DISPLAY']=':1.0'
+
     assert os.system("python ./scripts/figures/Figure1.py") == 0
-    
