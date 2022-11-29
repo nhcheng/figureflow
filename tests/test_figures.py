d0331dbb031e487422a589bbb1c8198bd7d3f674
nhcheng <57288817+nhcheng@users.noreply.github.com>
2022-11-29 15:58:04 -0500

    add all figures

--- tests/test_figures.py
+++ tests/test_figures.py
@@ -8,4 +8,6 @@ def test_all():
     os.system('Xvfb :1 -screen 0 1600x1200x16  &')
     os.environ['DISPLAY']=':1.0'
 
-    assert os.system("python ./scripts/figures/Figure1.py") == 0
+    for i in range(1, 10 + 1):
+        assert os.system(f"python ./scripts/figures/Figure{i}.py") == 0
+
